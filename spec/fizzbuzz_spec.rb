e9edfa4366f5bc926d92799bea852cc488d6f633
savannaelbey <savanna_e@icloud.com>
2020-10-20 16:22:46 +0100

    final fizzbuzz spec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,13 +3,13 @@ describe "fizzbuzz" do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq "Fizz"
   end
-  it 'returns "fizz" when passed 5' do
+  it 'returns "Buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq "Buzz"
   end
-  it 'returns "fizz" when passed 15' do
+  it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq "FizzBuzz"
   end
-  it 'returns "fizz" when passed 1' do
+  it 'returns the number if its not a mutiple of 3 or 5' do
     expect(fizzbuzz(1)).to eq 1
   end
 end
